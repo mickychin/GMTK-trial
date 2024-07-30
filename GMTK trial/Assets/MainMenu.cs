b457fa5f78ad477e7f797703c4c420112e5bf558
Space-c2 <50313983+Space-c2@users.noreply.github.com>
2024-07-30 17:03:13 +0700

    MAIN MENU

---
+++ GMTK trial/Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+ // Replace "GameScene" with your game scene name
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
