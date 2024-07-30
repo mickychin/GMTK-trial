625d9ee1c4cf895414d5eaf2c472d972b340b04e
Space-c2 <50313983+Space-c2@users.noreply.github.com>
2024-07-30 15:38:45 +0700

    I've been a baaad girl

--- GMTK trial/Assets/Script/Money.cs
+++ GMTK trial/Assets/Script/Money.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Money : MonoBehaviour
 {
 
     [Header("Money")]
     public int Moneys;
-
+    public TextMeshProUGUI MoneysText;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,7 @@ public class Money : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoneysText.text = " : " + Moneys.ToString();
+
     }
 }
