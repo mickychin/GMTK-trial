0fa4ae246adc2b58b0e22a837804a5f1e7a56fb5
Space-c2 <50313983+Space-c2@users.noreply.github.com>
2024-07-30 15:26:17 +0700

    Mommy

--- GMTK trial/Assets/Script/Money.cs
+++ GMTK trial/Assets/Script/Money.cs
@@ -6,7 +6,7 @@ public class Money : MonoBehaviour
 {
 
     [Header("Money")]
-    public int Money;
+    public int Moneys;
 
     // Start is called before the first frame update
     void Start()
