f95ce738f483799ea0b5ba9aa3dcd108bbdcc082
Space-c2 <50313983+Space-c2@users.noreply.github.com>
2024-07-30 12:03:00 +0700

    Child Collision

--- GMTK trial/Assets/ChildrenAI.cs
+++ GMTK trial/Assets/ChildrenAI.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class ChildrenAI : MonoBehaviour
 {
-
-    public float speed = 5f; 
+    [Header("Stats")]
+    public float speed = 1f; 
     private Vector3 direction; 
+    private Vector3 castStartPosition; 
 
+    [Header("Collision")]
+    public LayerMask collisionLayer;
+    public float checkRadius = 0.5f;
 
     void Start()
     {
@@ -17,12 +21,26 @@ public class ChildrenAI : MonoBehaviour
     void Update()
     {
         transform.Translate(direction * speed * Time.deltaTime);
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, checkRadius, direction, 0.1f, collisionLayer);
+        castStartPosition = transform.position;
+
+        if (hit.collider != null)
+        {
+            direction = GetRandomDirection();
+        }
     }
 
     Vector3 GetRandomDirection()
     {
         float angle = Random.Range(0f, Mathf.PI * 2);
-        
-        return new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)).normalized;
+        return new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0).normalized;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(castStartPosition, checkRadius);
+        Gizmos.DrawLine(castStartPosition, castStartPosition + direction * 0.1f);
+        Gizmos.DrawWireSphere(castStartPosition + direction * 0.1f, checkRadius);
     }
 }
