e1001cbc96e390bd57ecb87d45db54ca899c13ca
mickychin <chinkrittan@hotmail.com>
2024-07-30 12:29:32 +0700

    Added a loop and spawner

--- GMTK trial/Assets/Script/ChildSpawner.cs
+++ GMTK trial/Assets/Script/ChildSpawner.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ChildSpawner : MonoBehaviour
 {
+    public GameObject child;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnChild());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator SpawnChild()
+    {
+        Debug.Log("HELLO");
+        yield return new WaitForSeconds(5f);
+        StartCoroutine(SpawnChild());
     }
 }
